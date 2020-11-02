35d3dff25874c3f94aeff3a97a7391ebbbac036b
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2020-11-02 18:48:28 +0530

    Update action.php

--- action.php
+++ action.php
@@ -6,53 +6,20 @@ $gender = $_POST['gender'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-if(!empty($fname) || !empty($lname) || !empty($email) || !empty($gender) || !empty($subject) || !empty($message))
+
+$conn = new mysqli('localhost','root', '');
+if($conn->connect_error)
 {
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "dataform";
-
-    //connection
-    $conn = new mysqli($host , $dbUsername , $dbPassword , $dbname);
-
-    if(mysqli_connect_error())
-    {
-        die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
-    }
-    else
-    {
-        $SELECT = "SELECT email From register Where email = ? Limit 1";
-        $INSERT = "INSERT Into register (fname , lname , email, gender , subject , message) values(?,?,?,?,?,?)";
-
-        //prepare statement
-        $stmt = $conn->prepare($SELECT);
-        $stmt ->bind_param("s",$email);
-        $stmt->execute();
-        $stmt ->bind_result($email);
-        $stmt ->store_result();
-        $rnum = $stmt->num_rows;
-
-        if($rnum == 0)
-        {
-            $stmt->close();
-            $stmt = $conn->prepare($INSERT);
-            $stmt ->bind_param($fname,$lname,$email,$gender,$subject,$message);
-            $stmt ->execute();
-            echo "New record inserted Sucessfully";
-        }
-        else
-        {
-            echo "Somebody already registor using this email";
-        }
-        $stmt ->close();
-        $conn ->close();
-    }
+    die('Connection Failed : '.$conn->connect_error);
 }
 else
 {
-    echo "All Filled are required";
-    die();
-}
+    $stmt = $conn->prepare("insert into form(fname,lname,email,gender,subject,message) values(?,?,?,?,?,?)");
+    $stmt->bind_param("ssssss",$fname,$lname,$email,$gender,$subject,$message);
+    $stmt->execute();
+    echo "Message Sent....";
+    $stmt->close();
+    $conn->close();
 
+}
 ?>
