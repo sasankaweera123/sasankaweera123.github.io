561b567ed1e4a62c321f8725c6f3f2f57a4242d7
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2020-11-02 19:08:11 +0530

    edited

--- action.php
+++ action.php
@@ -1,26 +1,26 @@
 <?php
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$gender = $_POST['gender'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
 
-$conn = new mysqli('localhost','root', '','dataform');
-if($conn->connect_error)
-{
-    die('Connection Failed : '.$conn->connect_error);
-}
-else
-{
-    $stmt = $conn->prepare("insert into form(fname,lname,email,gender,subject,message) values(?,?,?,?,?,?)");
-    $stmt->bind_param("ssssss",$fname,$lname,$email,$gender,$subject,$message);
-    $stmt->execute();
-    echo "Message Sent....";
-    $stmt->close();
-    $conn->close();
+    $conn = new mysqli('localhost','root', '','dataform');
+    if($conn->connect_error)
+    {
+        die('Connection Failed : '.$conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("insert into form(fname,lname,email,gender,subject,message) values(?,?,?,?,?,?)");
+        $stmt->bind_param("ssssss",$fname,$lname,$email,$gender,$subject,$message);
+        $stmt->execute();
+        echo "Message Sent....";
+        $stmt->close();
+        $conn->close();
 
-}
+    }
 ?>
