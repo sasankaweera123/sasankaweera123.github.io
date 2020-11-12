e5fda49a068fde59bad9a0f63e0ca29e6d4bef21
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2020-11-12 20:06:06 +0530

    edited

--- MyPortfolio/dataform.php
+++ MyPortfolio/dataform.php
@@ -1,26 +1,41 @@
 <?php
+            $hostname ="localhost";
+            $username ="root";
+            $password ="";
+            $dbname ="dataform";
+            
+            $con = new mysqli($hostname,$username,$password,$dbname);
 
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $gender = $_POST['gender'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+            if($con->connect_error)
+            {
+                die("Connection Falid !".$con->connect_error);
+            }
+            else
+            {
+                //echo ("Connection Successfull"."<br>");
+            }
 
+            if(isset($_POST['submit']))
+            {
+                
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                $gender = $_POST['gender'];
+                $subject = $_POST['subject'];
+                $message = $_POST['message'];
 
-    $conn = new mysqli('localhost','root', '','dataform');
-    if($conn->connect_error)
-    {
-        die('Connection Failed : '.$conn->connect_error);
-    }
-    else
-    {
-        $stmt = $conn->prepare("insert into form(fname,lname,email,gender,subject,message) values(?,?,?,?,?,?)");
-        $stmt->bind_param("ssssss",$fname,$lname,$email,$gender,$subject,$message);
-        $stmt->execute();
-        echo "Message Sent....";
-        $stmt->close();
-        $conn->close();
+                $data = "INSERT INTO form(fname,lname,email,gender,subject,message) VALUES('$fname','$lname','$email','$gender','$subject','$message')";
 
-    }
-?>
+                if(mysqli_query($con,$data))
+                {
+                //echo "Contact Saved" . "<br>"."Thank You";
+                }
+                else
+                {
+                    echo "Error ".$data."</br>".mysqli_error($con);
+                }
+            }
+            mysqli_close($con);
+            
+    ?>
