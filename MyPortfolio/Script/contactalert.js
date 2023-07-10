6492e1322bd0cfcc8fd59562fb5d766c4423b8b0
sasankaweera123 <sajewsasanka1@gmail.com>
2023-07-11 00:03:26 +0530

    Update contactalert.js

--- MyPortfolio/Script/contactalert.js
+++ MyPortfolio/Script/contactalert.js
@@ -1,7 +1,3 @@
-function display() {
-    if (document.getElementById('fname').value == "" || document.getElementById('lname').value == "" || document.getElementById('email').value == "" || document.getElementById('subject').value == "") {
-        alert("Please Fill the below Fields properly!!");
-    } else {
-        alert("Message Saved ,  Thank you");
-    }
-}
+function showEmailSentAlert() {
+    alert("Email sent");
+  }
